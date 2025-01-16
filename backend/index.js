b58276748f22231ef87bdd266ea307417c1ff6ef
JatinMehta007 <mehtajatin045@gmail.com>
2025-01-17 00:43:17 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000
 const cors = require('cors');
 const rootRouter = require("./routes/index");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1", rootRouter);
+app.use("/api/v1",  rootRouter);
 
-module.exports = app; // Export the app instead of listening directly
+app.listen(PORT, () => {
+    console.log("Server running on port",PORT);
+});
