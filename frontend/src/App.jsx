587da35196c53aa7e5b11fb666bf28d67a2e4d77
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-06 12:03:02 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from "./components/home"
 import { Signup } from "./components/signup"
 import { Admin } from "./components/Admin"
 import { Patient } from "./sub-components/Patient"
+import { ExpensePredictor } from "./components/ExpensePredictor"
 import { Record } from "./sub-components/Record"
 import { MealDetails } from "./sub-components/MealDetails"
 import { FullDetails } from "./sub-components/FullDetails"
@@ -21,6 +22,7 @@ function App() {
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/admin" element={<Admin/>}></Route>
         <Route path="/patient" element={<Patient/>}></Route>
+        <Route path="/predict" element={<ExpensePredictor />}></Route> 
         <Route path="/record" element={<Record/>}></Route>
         <Route path="/mealdetails" element={<MealDetails/>}></Route>
         <Route path="/full" element={<FullDetails/>}></Route>
