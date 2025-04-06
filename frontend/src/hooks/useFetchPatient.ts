7bdc89993c9b907cba3572362da45b77a7737eea
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-06 12:00:48 +0530

    add useFetch_Hook

---
+++ frontend/src/hooks/useFetchPatient.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
+
+export const useFetchPatient = () => {
+  const [patients, setPatients] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchPatients = async ()=>{
+        setLoading(true);
+      try{
+        const response = await axios.get(`${BACKEND_URL}/api/v1/user/patients`);
+        console.log("Fetched patients:", response.data);
+        setPatients(response.data);
+      } catch(error){
+        console.log("error fetching patient data",error);
+      } finally{
+        setLoading(false);
+      }
+    }
+
+    fetchPatients();
+  },[])
+
+
+  return { patients, loading };
+};
