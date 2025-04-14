93546529cae6dd0dda62a0a58c5a36e53770f6ff
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-14 19:27:52 +0530

    update

--- frontend/src/sub-components/Patient.jsx
+++ frontend/src/sub-components/Patient.jsx
@@ -46,10 +46,10 @@ export const Patient = () => {
                 >
                   <div className="group-hover:bg-yellow-600 bg-zinc-800 w-[10px] h-14 pl-0"></div>
                   <div className="w-full flex flex-row">
-                    <p className="pl-10 h-14 w-full  uppercase font-bold bg-zinc-800 text-zinc-200 text-sm ">
+                    <div className="pl-10 h-14 w-full  uppercase font-bold bg-zinc-800 text-zinc-200 text-sm ">
                       <p className="normal-case font-light pb-1 mt-1">Patient Name</p>
                       {patient.username} {/* Display the patient's name */}
-                    </p>
+                    </div>
                     <p className="ml-20 text-zinc-400 p-2 font-semibold text-sm bg-zinc-800 cursor-pointer hover:text-white">
                       Click to add the meal
                     </p>
