30add19c9b32cc51cb2b3bf10f0ba689730b6a70
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-15 00:38:48 +0530

    update UI

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -19,19 +19,20 @@ export const Signup=()=>{
     return(
         
 
-        <div className="bg-zinc-900  w-full h-screen flex justify-center">
+        <div className="bg-zinc-900  w-full h-screen flex justify-center ">
             <div className= "w-72 bg-zinc-950 border absolute mt-52 rounded-2xl  border-transparent ">
-                <div className="p-6">
+            <p className="text-white uppercase text-center font-medium tracking-wide mt-2">Manager Username</p>
+                <div className="p-6 ml-2 tracking-wider">
                 <p className="text-white font-medium ">
                 Username
-                <input type="text" placeholder="heliverse123@xyz.com" className="mt-1 font-normal rounded-lg bg-zinc-950 border p-2 " value={username} onChange={(e)=>setUsername(e.target.value)}/>
+                <input type="text" placeholder="heliverse123@xyz.com" className="mt-1 font-normal rounded-lg bg-zinc-950 border p-2 w-56 " value={username} onChange={(e)=>setUsername(e.target.value)}/>
                 </p>
-                <p className="text-white mt-2 font-medium">
+                <p className="text-white mt-4 font-medium">
                 Password
-                <input type="password"  placeholder="*********" className="mt-1 font-normal rounded-lg bg-zinc-950 border p-2 "  value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <input type="password"  placeholder="**************" className="mt-1 font-normal rounded-lg bg-zinc-950 border p-2 w-56 "  value={password} onChange={(e)=>setPassword(e.target.value)}/>
                 </p>
                 {error && <p className="text-red-500 mt-2 text-sm">{error}</p>}
-                <button className="text-black ml-20 mt-10 bg-white p-1 rounded-md font-medium" onClick={Signup} > 
+                <button className="text-black ml-20 mt-10 bg-zinc-300 hover:bg-white p-1 rounded-md font-medium" onClick={Signup} > 
                     Signup
                 </button>
                 </div>
