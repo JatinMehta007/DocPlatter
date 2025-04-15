1b7afab4878c49fe34345a5ab5692e2ba5e5e49e
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-15 14:59:49 +0530

    add deleteOption

--- frontend/src/sub-components/showMeal.jsx
+++ frontend/src/sub-components/showMeal.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../../config";
 import { AuroraBackground } from "../ui/aurora_background";
 import { motion } from "framer-motion";
+import { DeleteMeals } from "../components/skeleton/deleteMutipleMeal";
 
 export const ShowMealDetails = ({ patientName }) => {
   const [patientMeals, setPatientMeals] = useState([]);
@@ -31,6 +32,7 @@ export const ShowMealDetails = ({ patientName }) => {
   }, [patientName]);
 
   return (
+    <div>
     <AuroraBackground>
        <motion.div
         initial={{ opacity: 0.0, y: 40 }}
@@ -41,33 +43,40 @@ export const ShowMealDetails = ({ patientName }) => {
           ease: "easeInOut",
         }}
         className=""
-      >
+        >
     <div className="text-white m-10 rounded-lg  p-6 w-[500px]">
       <p className="font-bold text-4xl text-center pt-4 mb-8">
-        Meal for <span className="text-amber-400">{patientName}</span>
+        Meal for <span className="text-amber-400 uppercase">{patientName}</span>
       </p>
-
+       
       {patientMeals.length > 0 ? (
-        patientMeals.map((meal, index) => (
-          <div
-            key={index}
+        patientMeals.map((meal, index) => {
+          console.log("tje redndoreorm:",meal);
+          return (
+            <div
+            key={meal.id || index}
             className="bg-gray-800 rounded-lg p-4 mb-4 border border-gray-600 "
-          >
-            <p className="text-base  tracking-wider text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Morning : </strong> {meal.morning_meal || "null"}</p>
+            >
+              <div className="ml-[400px]  absolute ">
+            <DeleteMeals id={meal.id}/>
+          </div>
+            <p className="text-base font-medium  tracking-wider text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Morning : </strong> {meal.morning_meal || "null"}</p>
             <p className="text-base  tracking-wider  text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Date : </strong> {meal.date || "null"}</p>
             <p className="text-base  tracking-wider  text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Evening : </strong> {meal.evening_meal || "null"}</p>
             <p className="text-base  tracking-wider  text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Night : </strong> {meal.night_meal || "null"}</p>
             <p className="text-base  tracking-wider  text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Ingredients : </strong> {meal.ingredients || "null"}</p>
             <p className="text-base  tracking-wider  text-amber-600 uppercase"><strong className="text-amber-400 text-lg tracking-normal ">Instruction : </strong> {meal.instruction || "null"}</p>
           </div>
-        ))
+          )
+        })
       ) : (
         <p className="text-center text-gray-400">
           No meal data found for <strong>{patientName}</strong>
         </p>
-      )}
-    </div>
+        )}
+        </div>
     </motion.div>
     </AuroraBackground>
+      </div>
   );
 };
