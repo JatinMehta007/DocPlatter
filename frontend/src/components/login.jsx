91ccdf0109e1f3d06f292b08f1cb06c4b30ca5e1
Jatin_Mehta <mehtajatin045@gmail.com>
2025-05-25 00:12:49 +0530

    add spotlight

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -62,9 +62,9 @@ export const Login = () => {
   }
 
   return (
-     <HeroHighlight containerClassName="bg-zinc-900 dark:bg-zinc-950  overflow-hidden">
     <div className=" w-full h-screen flex justify-center  bg-gradient-to-r from-zinc-900  to-zinc-950 overflow-hidden ">
-    
+    <Spotlight className="-top-[90px]  left-0 md:-top-3 md:left-44 md:h-[1000px] lg:h-[1400px]"
+        fill="white"></Spotlight>
       <div className="lg:w-[400px] w-[360px] bg-zinc-950 border absolute mt-52 rounded-lg  border-zinc-600 ">
         <p className="text-white  text-center font-bold tracking-wide mt-2 text-xl">
           Join DocPlatter
@@ -116,6 +116,5 @@ export const Login = () => {
         </div>
       </div>
     </div>
-    </HeroHighlight>
   );
 };
