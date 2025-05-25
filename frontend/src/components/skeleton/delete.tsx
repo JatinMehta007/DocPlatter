6765a13abb18f18e998b6f6225545572658f3761
Jatin_Mehta <mehtajatin045@gmail.com>
2025-05-25 17:57:21 +0530

    adjust height

--- frontend/src/components/skeleton/delete.tsx
+++ frontend/src/components/skeleton/delete.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import { BACKEND_URL } from "../../../config";
 import { AnimatedTooltip } from "../../ui/popup";
 import { Spinner } from "./spinner";
@@ -43,19 +43,18 @@ export const DeletePatient = ({ id }: { id: Number }) => {
   ];
   
   return (
-    <div className="ml-auto relative ">
+    <div className="md:ml-auto h-14   p-1 sm:p-0   relative  ">
       {loading && (
         <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/20" ><Spinner/></div>
       )}
       <button onClick={handleDelete} >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="50"
-          height="50"
-          viewBox="0 0 256 256"
-          className="hover:bg-yellow-200  rounded-sm absolute"
-        >
-          <g
+        
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0,0,256,256"
+  className="hover:bg-yellow-200 md:w-[50px] md:h-[50px] h-[40px] w-[40px]  rounded-sm absolute"
+>
+  <g
             fill="none"
             fillRule="nonzero"
             stroke="none"
@@ -63,27 +62,31 @@ export const DeletePatient = ({ id }: { id: Number }) => {
             strokeLinecap="butt"
             strokeLinejoin="miter"
             strokeMiterlimit="10"
-          >
-            <g transform="scale(5.33333,5.33333)">
-              <path
-                d="M34,12l-6,-6h-8l-6,6h-3v28c0,2.2 1.8,4 4,4h18c2.2,0 4,-1.8 4,-4v-28z"
-                fill="#e7b217"
-              ></path>
-              <path
-                d="M24.5,39h-1c-0.8,0 -1.5,-0.7 -1.5,-1.5v-19c0,-0.8 0.7,-1.5 1.5,-1.5h1c0.8,0 1.5,0.7 1.5,1.5v19c0,0.8 -0.7,1.5 -1.5,1.5zM31.5,39v0c-0.8,0 -1.5,-0.7 -1.5,-1.5v-19c0,-0.8 0.7,-1.5 1.5,-1.5v0c0.8,0 1.5,0.7 1.5,1.5v19c0,0.8 -0.7,1.5 -1.5,1.5zM16.5,39v0c-0.8,0 -1.5,-0.7 -1.5,-1.5v-19c0,-0.8 0.7,-1.5 1.5,-1.5v0c0.8,0 1.5,0.7 1.5,1.5v19c0,0.8 -0.7,1.5 -1.5,1.5z"
-                fill="#04010b"
-              ></path>
-              <path
-                d="M11,8h26c1.1,0 2,0.9 2,2v2h-30v-2c0,-1.1 0.9,-2 2,-2z"
-                fill="#090116"
-              ></path>
-            </g>
-          </g>
-        </svg>
-        <div className="">
+  >
+    <g transform="scale(5.33333,5.33333)">
+      <path
+        d="M34,12l-6,-6h-8l-6,6h-3v28c0,2.2 1.8,4 4,4h18c2.2,0 4,-1.8 4,-4v-28z"
+        fill="#e7b217"
+      ></path>
+      <path
+        d="M24.5,39h-1c-0.8,0 -1.5,-0.7 -1.5,-1.5v-19c0,-0.8 0.7,-1.5 1.5,-1.5h1c0.8,0 1.5,0.7 1.5,1.5v19c0,0.8 -0.7,1.5 -1.5,1.5zM31.5,39v0c-0.8,0 -1.5,-0.7 -1.5,-1.5v-19c0,-0.8 0.7,-1.5 1.5,-1.5v0c0.8,0 1.5,0.7 1.5,1.5v19c0,0.8 -0.7,1.5 -1.5,1.5zM16.5,39v0c-0.8,0 -1.5,-0.7 -1.5,-1.5v-19c0,-0.8 0.7,-1.5 1.5,-1.5v0c0.8,0 1.5,0.7 1.5,1.5v19c0,0.8 -0.7,1.5 -1.5,1.5z"
+        fill="#04010b"
+      ></path>
+      <path
+        d="M11,8h26c1.1,0 2,0.9 2,2v2h-30v-2c0,-1.1 0.9,-2 2,-2z"
+        fill="#090116"
+      ></path>
+    </g>
+  </g>
+</svg>;
+
+        <div className="md:mt-[-20px] invisible lg:visible">
         <AnimatedTooltip items={people} />
         </div>
       </button>
     </div>
   );
 };
+
+
+
