126b9a2aa0416763d9da05ac5c0e555f440ebf76
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-07 00:01:58 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,22 @@ const app = express();
 // app.use(cors({
 //     origin: '*' 
 // }));
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://docplatter-jatinmehta.vercel.app" 
+];
 
 app.use(cors({
-    origin: "https://docplatter-jatinmehta.vercel.app/",  
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 
 app.use("/api/v1",  rootRouter);
